3df19012c8133ed385dc5097d86cc30e01ab074a
Dorynn <dolinha1k12@gmail.com>
2024-04-15 22:50:22 +0700

    update file structure

--- bds.fe/src/app/app-routing.module.ts
+++ bds.fe/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './modules/main/admin/admin.component';
+import { MainComponent } from './modules/main/main.component';
+import { UserComponent } from './modules/main/user/user.component';
+import { HomepageComponent } from './pages/user/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: 'user',
+        component: UserComponent,
+      },
+      {
+        path: 'user',
+        children: [
+          {
+            path: 'homepage',
+            component: HomepageComponent
+          }
+        ]
+      },
+      {
+        path: 'admin',
+        component: AdminComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bds.fe/src/app/app.module.ts
+++ bds.fe/src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/main/header/header.component';
+import { FooterComponent } from './modules/main/footer/footer.component';
+import { AdminComponent } from './modules/main/admin/admin.component';
+import { UserComponent } from './modules/main/user/user.component';
+import { MainComponent } from './modules/main/main.component';
+import { LoginComponent } from './modules/login/login.component';
+import { RegisterComponent } from './modules/register/register.component';
+import { HomepageComponent } from './pages/user/homepage/homepage.component';
+import { DashboardComponent } from './pages/admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AdminComponent,
+    UserComponent,
+    MainComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomepageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
